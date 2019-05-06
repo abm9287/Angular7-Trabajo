f8abd807fb636903774aa653e52f317d143b4797
johao70 <perlazajohao@gmail.com>
2019-05-06 13:35:09 -0500

    models knex

---
+++ Angular/src/app/models/anulacion.ts
@@ -0,0 +1,19 @@
+export class Anulacion{
+
+    id_anulacion: Number;
+    nombres: String;
+    apellidos: String;
+    fecha_nacimiento: Date;
+    cedula: String;
+    sexo: String;
+    provincia: String;
+    canton: String;
+    parroquia: String;
+    direccion: String;
+    celular: String;
+    correo: String;
+    etnia: String;
+    titulo_bachiller: String;
+    fecha_titulacion: Date;
+        
+}
---
+++ Angular/src/app/models/inscripcion.ts
@@ -0,0 +1,19 @@
+export class Inscripcion{
+
+    id_inscripcion: Number;
+    nombres: String;
+    apellidos: String;
+    fecha_nacimiento: Date;
+    cedula: String;
+    sexo: String;
+    provincia: String;
+    canton: String;
+    parroquia: String;
+    direccion: String;
+    celular: String;
+    correo: String;
+    etnia: String;
+    titulo_bachiller: String;
+    fecha_titulacion: Date;
+        
+}
